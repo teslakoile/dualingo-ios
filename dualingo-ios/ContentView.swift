459b00209fabcf57965c990e9e2cbf449fb3b5d8
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2023-12-26 23:00:38 +0800

    Initial Commit

---
+++ dualingo-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dualingo-ios
+//
+//  Created by Kyle Naranjo on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dualingo-ios/dualingo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dualingo_iosApp.swift
+//  dualingo-ios
+//
+//  Created by Kyle Naranjo on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct dualingo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
